e37d496bcf82a3360c25309e23bdf46e224f9d42
CobyLsy <cobylinlsy@gmail.com>
2024-06-03 17:40:42 -0700

    Update Clustering.R to the correct, final version

--- Clustering.R
+++ Clustering.R
@@ -1,14 +1,15 @@
 
+library(ape)
+library(cluster)
 
 # Read the CSV file
 similarity_matrix <- read.csv("/Users/siyanlin/Documents/GitHub/CSE185-Final-Project/symmetric_matrix_Normalized.csv")
 dissimilarity_matrix <- read.csv("/Users/siyanlin/Documents/GitHub/CSE185-Final-Project/dissimilarity_matrix.csv")
 # Convert to a matrix (if needed)
 similarity_matrix <- as.matrix(similarity_matrix)
-dissimilarity_matrix <- as.matrix(dissimilarity_matrix)
+dissimilarity_matrix <- as.matrix(dissimilarity_matrix[, -1])
 
-
-distance_object <- as.dist(dissimilarity_matrix[, -1])
-
-hc <- hclust(distance_object, method = "ward.D2")
-
+aggTree <- agnes(as.dist(dissimilarity_matrix), diss = T, method = "ward")
+aggDend <- as.hclust(aggTree)
+phy <- as.phylo(aggDend)
+write.tree(phy, file = "TreeOutputDiss")
